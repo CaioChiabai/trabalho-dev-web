0d59490ae9a70a23a04b29169c9efec4c3027265
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-05-31 10:49:53 -0300

    feat: iniciado projeto frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CadastroRestaurante from './pages/CadastroRestaurante'
+
+function App() {
+
+  return (
+    <>
+      <CadastroRestaurante />
+    </>
+  )
+}
+
+export default App
